7cb1d2ff4f043ab1ff6e29720d9691839410463e
Krzysztof Rygielski <krzysztof@rygiel.net>
2017-04-10 22:38:51 +0200

    removed puts

--- lib/zoomba/collection.rb
+++ lib/zoomba/collection.rb
@@ -2,8 +2,6 @@
 module Zoomba
   class Collection < Array
     def initialize(api_response, type = nil)
-      puts api_response
-
       super(api_response[type.to_s].map { |u| Zoomba::User.new(u) })
     end
   end
